756e8d5012fa3271900a888868106f0f86b18ab4
kawilliams <kawilliams@email.arizona.edu>
2021-06-29 09:04:43 -0400

    JS version

--- script/upload-face.js
+++ script/upload-face.js
@@ -1,6 +1,38 @@
 console.log("upload-face.js");
+/*
+form name='faceUploadForm'
+input id='myfile', name='fileToUpload'
+input id='uploadImage', name='submit', value='Upload Image'
+*/
 
-var svgID = "uploadSVG";
-var divID = "uploadFace";
+const svgId = "uploadSVG";
+const divId = "uploadFace";
+// const formName = "faceUpload";
+const fileId = "myfile";
+// const submitId = "uploadImage";
 
-const selectedFile = document.getElementById('uploadImage').files[0];
+const target_dir = 'uploaded-faces/';
+var uploadOk = 1;
+
+// const fileInput = document.getElementById("uploadImage");
+const fileElem = document.getElementById(fileId);
+
+function submitImage() {
+	console.log("Image submitted", this.files);
+	var div = document.getElementById('imageContainer');
+	//Clear existing elements
+	while (div.firstChild) { div.removeChild(div.firstChild); }
+	//Only accept the first file
+	var file = this.files[0];
+	console.log("name", file.name);
+	//Check file size
+	if (file.size > 5000000) {
+		console.log("Sorry, your file is too large.");
+		uploadOk = 0;
+	}
+
+}
+
+
+/// Listen for uploads
+fileElem.addEventListener("change", submitImage, false);
