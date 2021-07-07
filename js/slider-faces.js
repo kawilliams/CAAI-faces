0650ed6a61104188c59237075d3f3933f130b239
kawilliams <kawilliams@email.arizona.edu>
2021-07-07 15:34:07 -0400

    Style slider vis and add prediction score

--- js/slider-faces.js
+++ js/slider-faces.js
@@ -17,40 +17,8 @@ var predVal = {
 	y: 100
 }
 
-var prefix = './slider-faces/renamed_images/seed_200_var_';
-var initImage = './slider-faces/renamed_images/seed_200_var_bddd.png';
-
-var svg = d3.select('#sliderSVG')
-	.attr('preserveAspectRatio', 'xMidYMid meet')
-	.attr('viewBox', '0 0 ' + svgDimensions.width + ' ' +svgDimensions.height);
-	// .attr('style', 'outline: thin solid #48A9C5;');
-
-var imageG = svg.append('g')
-	.attr('id', 'imageG')
-	.attr('x', margin.left)
-	.attr('y', margin.right);
-
-var image = imageG.append('svg:image')
-	.attr('x', imageDimensions.x)
-	.attr('y', imageDimensions.y)
-	.attr('id', 'electionImage')
-	.attr('width', imageDimensions.width)
-	.attr('height', imageDimensions.height)
-	.attr('class', 'slider')
-	.attr('xlink:href', initImage);
-
-imageG.append('text')
-	.attr('x', predVal.x)
-	.attr('y', predVal.y-16)
-	.text('Value:')
-	.attr('font-size', '10em');
-
-var predictedValue = imageG.append('text')
-	.attr('id', 'prediction')
-	.attr('x', predVal.x)
-	.attr('y', predVal.y)
-	.text('0')
-	.attr('font-size', '10em');
+var directory = './slider-faces/renamed_images/';
+var topImage = './slider-faces/renamed_images/seed_200_var_bddd.png';
 
 const valueCode = {
 	'-1' : 'a',
@@ -64,19 +32,29 @@ const valueCode = {
 	'1' : 'i'
 }
 
-d3.csv("./slider-faces/metadata/meta_combined.csv").then(function(data){
+d3.csv("./slider-faces/metadata/meta_renamed.csv").then(function(data){
 
 	function transition(thisSlider) {
-		d3.select("#prediction").text(thisSlider.value);
+	
 		var val1 = document.getElementById("slider_1").value;
 		var val2 = document.getElementById("slider_2").value;
 		var val3 = document.getElementById("slider_3").value;
 		var val4 = document.getElementById("slider_4").value;
 
 		var imageCode = valueCode[val1]+valueCode[val2]+valueCode[val3]+valueCode[val4];
-		var nextImage = prefix + imageCode + '.png';
-		d3.select('#electionImage')
-			.attr('xlink:href', nextImage);
+		var nextImage = 'seed_200_var_' + imageCode + '.png';
+		var fullImagePath = directory + nextImage;
+		var oldImage = document.getElementById('topImage').src;
+		document.getElementById('topImage').src = fullImagePath;
+		document.getElementById('bottomImage').src = oldImage;
+		document.getElementById('bottomImage').style.opacity = '0.5';
+
+		//Retrieve image prediction score
+		for (var j=0; j<data.length; j++) {
+			if (data[j].renamed_img === (nextImage)) {
+				document.getElementById("prediction").innerHTML = data[j].pred_release;
+			}
+		}
 		
 	}
 
