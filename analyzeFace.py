07d5b961f278ed2580f4135764acc6a5b402e441
kawilliams <kawilliams@email.arizona.edu>
2021-07-08 09:13:56 -0400

    initial commit for py

---
+++ analyzeFace.py
@@ -0,0 +1,18 @@
+#analyzeFace.py
+"""
+Flask script to call functions to analyze images.
+
+Needs to install Flask in a virtual environment.
+
+"""
+
+
+def main():
+
+
+
+	return 
+
+
+if __name__ == '__main__':
+	main()
