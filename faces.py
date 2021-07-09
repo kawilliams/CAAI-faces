7e9d69db6b2712b07c6bd4d7360b1293e808e4aa
kawilliams <kawilliams@email.arizona.edu>
2021-07-09 10:42:02 -0400

    Remove form tags from Booth html, get upload working

--- faces.py
+++ faces.py
@@ -4,9 +4,13 @@ Flask script to call functions to analyze images.
 
 Installed Flask in a virtual environment (venv/).
 
+[ ] Limit file size
+[ ] Only allow certain extensions
+[ ] Use secure_filename() around filename
+
 """
 import os
-from flask import Flask, flash, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'static/uploaded-faces'
@@ -14,27 +18,50 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024*1024
 
 def evaluateImage():
 	prediction = 2222222
 	return prediction
 
-@app.route("/", methods=['POST','GET'])
-def showGANPrediction(yourPrediction=None):
+@app.route("/")
+def index():
+	return render_template('booth-faces.html')
+
+@app.route("/", methods=["POST"])
+def upload_file():
 	error = None
-	if request.method == 'POST':
-		if 'file' not in request.files:
-			flash('No file attached')
-			return redirect(request.url)
-		f = request.files['file']
-		if f.filename == '':
-			flash('No file selected')
-			return redirect(request.url)
-		if f and allowed_file(f.filename):
-			filename = secure_filename(f.filename)
-			f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('download_file'), name=filename)
+	if 'fileToUpload' not in request.files:
+		error = 'No file attached'
+		return redirect(url_for('index'))
+	
+	upload_file = request.files['fileToUpload']
+	if upload_file.filename != '':
+		upload_file.save(os.path.join(app.config['UPLOAD_FOLDER'], upload_file.filename))
+		yourPrediction = evaluateImage()
+	return render_template('booth-faces.html', error=error, yourPrediction=yourPrediction)
+
+
+# @app.route("/", methods=['POST']) #,'GET'])
+# def showGANPrediction(yourPrediction=None):
+# 	error = None
+# 	if request.method == 'POST':
+# 		print("KATY: post")
+# 		if 'file' not in request.files:
+# 			#flash('No file attached')
+# 			return redirect(request.url)
+# 		f = request.files['file']
+# 		if f.filename == '':
+# 			#flash('No file selected')
+# 			return redirect(request.url)
+# 		if f and allowed_file(f.filename):
+# 			print("KATY: we have a filename")
+# 			filename = secure_filename(f.filename)
+# 			f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+# 			print("SAVED the file", filename)
+# 			return redirect(url_for('index'), yourPrediction=yourPrediction, error=error)
 
 	
-	yourPrediction = evaluateImage()
-	return render_template('booth-faces.html', yourPrediction=yourPrediction, error=error)
+# 	yourPrediction = evaluateImage()
+# 	return render_template('booth-faces.html', yourPrediction=yourPrediction, error=error)
+	
