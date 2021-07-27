ef7b13714b385e968b56fae0422aba6587007930
kawilliams <kawilliams@email.arizona.edu>
2021-07-27 09:33:42 -0400

    Add loading messages to upload picture

--- static/js/upload-face.js
+++ static/js/upload-face.js
@@ -5,10 +5,28 @@ input id='uploadImage', name='submit', value='Upload Image'
 */
 
 
-function submitImage() {
-	document.getElementById("faceUploadForm").submit();
-}
-
 const fileElem = document.getElementById("myfile");
+
 /// Listen for uploads
 fileElem.addEventListener("change", submitImage, false);
+
+
+function submitImage() {
+	document.getElementById("faceUploadForm").submit();
+	var imageText = document.getElementById("imageLoading");
+	const loadMessages = ["Submitted image...", "Checking if CNN can process image...", "Processing image..."];
+
+	function sleep(ms) {
+  		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	async function showLoadingMessages() {
+	  imageText.innerHTML = loadMessages[0];
+	  await sleep(1500);
+	  imageText.innerHTML = loadMessages[1];
+	  await sleep(1500);
+	  imageText.innerHTML = loadMessages[2];
+	}
+	
+	showLoadingMessages();
+}
